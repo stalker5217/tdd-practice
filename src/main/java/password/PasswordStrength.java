739eb7089ee7580d6e875799f18a59306499ed22
Song <stalker5217@gmail.com>
2021-02-23 16:31:14 +0900

    패스워드 강도

---
+++ src/main/java/password/PasswordStrength.java
@@ -0,0 +1,5 @@
+package password;
+
+public enum PasswordStrength {
+    INVALID, WEAK, NORMAL, STRONG
+}
---
+++ src/test/java/password/PasswordStrengthMeterTest.java
@@ -0,0 +1,77 @@
+package password;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 패스워드를 세 가지 조건으로 검증한다.
+ * 1. 길이가 8글자 이상
+ * 2. 0부터 9까지의 숫자 포함
+ * 3. 대문자를 포함
+ *
+ * 세 가지 모두 만족시 패스워드 강도는 '강함'
+ * 두 가지를 만족하면 '보통'
+ * 한 가지 이하를 만족하면 '약함'
+ */
+
+class PasswordStrengthMeterTest {
+    private final PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void nullInput_Then_Invalid() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    @Test
+    void emptyInput_Then_Invalid() {
+        assertStrength("", PasswordStrength.INVALID);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
+        assertStrength("ab12!@df", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOnlyLengthCriteria_Then_Weak() {
+        assertStrength("abdefghi", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyNumCriteria_Then_Weak() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyUpperCriteria_Then_Weak() {
+        assertStrength("ABZEF", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsNoCriteria_Then_Weak() {
+        assertStrength("abc", PasswordStrength.WEAK);
+    }
+}
